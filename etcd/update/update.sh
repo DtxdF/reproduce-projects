25edba9a21db1add66f01e48ea4388fd6058116b
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-08-15 13:25:53 -0400

    etcd: Update Etcd

--- etcd/update/update.sh
+++ etcd/update/update.sh
@@ -14,5 +14,4 @@ cat -- "${BASEDIR}/reproduce.conf.template" |\
         -e "s/%%TAG2%%/${TAG2}/g" \
         -e "s/%%ETCD1%%/${ETCD1}/g" \
         -e "s/%%ETCD2%%/${ETCD2}/g" \
-        -e "s/%%ETCD3%%/${ETCD3}/g" \
-        -e "s/%%ETCD4%%/${ETCD4}/g" > "${BASEDIR}/../reproduce.conf"
+        -e "s/%%ETCD3%%/${ETCD3}/g" > "${BASEDIR}/../reproduce.conf"

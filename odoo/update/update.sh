15aa319d3e721ccba471f107391609071dbb0af7
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-09-16 21:18:27 -0400

    Add odoo

---
+++ odoo/update/update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+. "${BASEDIR}/update.conf"
+
+set -xe
+set -o pipefail
+
+cat -- "${BASEDIR}/reproduce.conf.template" |\
+    sed -E \
+        -e "s/%%TAG1%%/${TAG1}/g" \
+        -e "s/%%TAG2%%/${TAG2}/g" \
+        -e "s/%%ODOO1%%/${ODOO1}/g" \
+        -e "s/%%ODOO2%%/${ODOO2}/g" > "${BASEDIR}/../reproduce.conf"

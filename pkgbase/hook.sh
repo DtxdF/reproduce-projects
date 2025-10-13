8363e872fc2afa4fb571d1e06057b6e3cf814f2c
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-10-12 20:19:07 -0400

    pkgbase: Remove hook

--- pkgbase/hook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-touch -- "${REPRODUCE_JAIL_NAME}/conf/config.conf"
-osversion=`appjail cmd chroot "${REPRODUCE_JAIL_NAME}" freebsd-version | grep -Eo '[0-9]+\.[0-9]+-[a-zA-Z0-9]+'`
-appjail-config set -Vt "${REPRODUCE_JAIL_NAME}/conf/config.conf" osarch="${REPRODUCE_OSARCH}"
-appjail-config set -Vt "${REPRODUCE_JAIL_NAME}/conf/config.conf" osversion="${osversion}"
-appjail-config set -Vt "${REPRODUCE_JAIL_NAME}/conf/config.conf" jail_type="thick"

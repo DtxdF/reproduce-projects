e8e70d6685e017b1b7f9f747a2394d201410d33d
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2025-12-04 14:19:47 -0400

    Update python

--- python/update/update.sh
+++ python/update/update.sh
@@ -11,4 +11,11 @@ set -o pipefail
 cat -- "${BASEDIR}/reproduce.conf.template" |\
     sed -E \
         -e "s/%%TAG1%%/${TAG1}/g" \
-        -e "s/%%TAG2%%/${TAG2}/g" > "${BASEDIR}/../reproduce.conf"
+        -e "s/%%TAG2%%/${TAG2}/g" \
+        -e "s/%%PYVER1%%/${PYVER1}/g" \
+        -e "s/%%PYVER2%%/${PYVER2}/g" \
+        -e "s/%%PYVER3%%/${PYVER3}/g" \
+        -e "s/%%PYVER4%%/${PYVER4}/g" \
+        -e "s/%%PYVER5%%/${PYVER5}/g" \
+        -e "s/%%PYVER6%%/${PYVER6}/g" \
+        -e "s/%%PYVER_MAJOR%%/${PYVER_MAJOR}/g" > "${BASEDIR}/../reproduce.conf"
